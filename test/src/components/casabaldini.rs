dd8dac046d4deaeb7dfe4c8ff75c440f81039ea4
carlozanieri <carlo.zanieri@gmail.com>
2026-02-19 22:40:42 +0100

    Versione 5.0

--- test/src/components/casabaldini.rs
+++ test/src/components/casabaldini.rs
@@ -1,31 +1,20 @@
 use crate::prelude::*;
 pub use crate::models::get_sliders_db;
-use crate::components::linkutili::Linkutili;
+
 use crate::components::elencosliders::ElencoSliders;
 #[component]
 pub fn Casabaldini(dir: String) -> Element {
-    // 1. TRUCCO CRUCIALE: Creiamo un segnale che si aggiorna SEMPRE 
-    // quando il componente riceve una nuova prop 'dir' dalla rotta.
-    // use_memo riesegue la chiusura ogni volta che 'dir' cambia.
-    let dir_memo = use_memo(move || dir.clone());
-
-    // 2. Ora la risorsa "osserva" il memo. 
-    // Poiché dir_memo() è un segnale reattivo, use_resource ripartirà 
-    // ogni volta che il valore del memo cambia.
-    let sliders_res = use_resource(move || async move {
-        // Leggendo il memo qui dentro, creiamo il legame reattivo
-        get_sliders_db(dir_memo()).await
-    });
-
-    // 3. Debug: questo apparirà nel terminale ad ogni cambio rotta
-    println!("Casabaldini sta visualizzando: {}", dir_memo());
-
+    println!("--- ESECUZIONE COMPONENTE ---");
+    let dir = use_signal(|| dir.to_string());
+    //let d_resource = dir.clone();
+    //let d = d_resource.clone();
+    let sliders = use_resource(move || get_sliders_db(dir.cloned()));
     rsx! {
-        div { class: "slider-pro",
-            hr {}
-            // Passiamo il valore attuale al figlio
-            ElencoSliders { dir: dir_memo() }
-            Linkutili {  } 
+            
+            div { class:"slider-pro", 
+            
+        hr {}
+            ElencoSliders {dir}
         }
     }
 }
