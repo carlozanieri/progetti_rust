389c669920676a454ef7c606ab5e418fe07390c4
carlozanieri <carlo.zanieri@gmail.com>
2026-02-19 18:04:50 +0100

    Versione 4.4

--- test/src/components/casabaldini.rs
+++ test/src/components/casabaldini.rs
@@ -4,8 +4,9 @@ pub use crate::models::get_sliders_db;
 use crate::components::elencosliders::ElencoSliders;
 #[component]
 pub fn Casabaldini(dir: String) -> Element {
-    
     let dir = use_signal(|| dir.to_string());
+    //let d_resource = dir.clone();
+    //let d = d_resource.clone();
     let sliders = use_resource(move || get_sliders_db(dir.cloned()));
     rsx! {
         
