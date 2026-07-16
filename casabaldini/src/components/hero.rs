bebe64dd5aa63406423587c3082f34ba8907f8f0
carlozanieri <carlo.zanieri@gmail.com>
2026-07-17 00:04:57 +0200

    Versione 11.4

--- casabaldini/src/components/hero.rs
+++ casabaldini/src/components/hero.rs
@@ -22,7 +22,15 @@ pub fn Hero() -> Element {
                 id: "header",
                 style: "width: 85%; height: auto;",
             }
-        
+
+            span { style: "font-size: 1.5em; color: #fffefe;",
+                "Barberino di Mugello a 2,5 Km. dall'uscita dell'Autostrada A1, a pochi Km. da Firenze "
+            
+            }
+            span { style: "font-size: 1.5em; color: #fffefe;",
+
+                b { "  Per informazioni e prenotazioni telefona al +39 3207060411" }
+            }
         }
     }
 }
